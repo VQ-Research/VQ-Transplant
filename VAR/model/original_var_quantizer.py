d4f1afe7250c23e8c1c73f7aa1285fa528a7f2ff
sunset-clouds <xianghong_fang@163.com>
2025-05-29 07:38:58 +0800

    initial

--- VAR/model/original_var_quantizer.py
+++ VAR/model/original_var_quantizer.py
@@ -4,13 +4,9 @@ import torch
 from torch import distributed as tdist, nn as nn
 from torch.nn import functional as F
 
-class VectorQuantizer2(nn.Module):
-    # VQGAN originally use beta=1.0, never tried 0.25; SD seems using 0.25
-    def __init__(
-        self, vocab_size, Cvae, using_znorm, beta: float = 0.25,
-        default_qresi_counts=0, v_patch_nums=None, quant_resi=0.5, share_quant_resi=4,  # share_quant_resi: args.qsr
-    ):
-        super().__init__()
+class Original_VAR(nn.Module):
+    def __init__(self, vocab_size, Cvae, using_znorm, beta: float = 0.25, v_patch_nums=None, quant_resi=0.5, share_quant_resi=4):
+        super(Original_VAR).__init__()
         self.vocab_size: int = vocab_size
         self.Cvae: int = Cvae
         self.using_znorm: bool = using_znorm
@@ -22,7 +18,7 @@ class VectorQuantizer2(nn.Module):
         self.beta: float = beta
         self.embedding = nn.Embedding(self.vocab_size, self.Cvae)
     
-    def forward(self, f_BChw: torch.Tensor, ret_usages=False) -> Tuple[torch.Tensor, List[float], torch.Tensor]:
+    def forward(self, f_BChw: torch.Tensor):
         dtype = f_BChw.dtype
         if dtype != torch.float32: f_BChw = f_BChw.float()
         B, C, H, W = f_BChw.shape
@@ -33,7 +29,44 @@ class VectorQuantizer2(nn.Module):
         
         with torch.cuda.amp.autocast(enabled=False):
             mean_vq_loss: torch.Tensor = 0.0
-            vocab_hit_V = torch.zeros(self.vocab_size, dtype=torch.float, device=f_BChw.device)
+            SN = len(self.v_patch_nums)
+            for si, pn in enumerate(self.v_patch_nums): # from small to large
+                # find the nearest embedding
+                if self.using_znorm:
+                    rest_NC = F.interpolate(f_rest, size=(pn, pn), mode='area').permute(0, 2, 3, 1).reshape(-1, C) if (si != SN-1) else f_rest.permute(0, 2, 3, 1).reshape(-1, C)
+                    rest_NC = F.normalize(rest_NC, dim=-1)
+                    idx_N = torch.argmax(rest_NC @ F.normalize(self.embedding.weight.data.T, dim=0), dim=1)
+                else:
+                    rest_NC = F.interpolate(f_rest, size=(pn, pn), mode='area').permute(0, 2, 3, 1).reshape(-1, C) if (si != SN-1) else f_rest.permute(0, 2, 3, 1).reshape(-1, C)
+                    d_no_grad = torch.sum(rest_NC.square(), dim=1, keepdim=True) + torch.sum(self.embedding.weight.data.square(), dim=1, keepdim=False)
+                    d_no_grad.addmm_(rest_NC, self.embedding.weight.data.T, alpha=-2, beta=1)  # (B*h*w, vocab_size)
+                    idx_N = torch.argmin(d_no_grad, dim=1)
+
+                # calc loss
+                idx_Bhw = idx_N.view(B, pn, pn)
+                h_BChw = F.interpolate(self.embedding(idx_Bhw).permute(0, 3, 1, 2), size=(H, W), mode='bicubic').contiguous() if (si != SN-1) else self.embedding(idx_Bhw).permute(0, 3, 1, 2).contiguous()
+                h_BChw = self.quant_resi[si/(SN-1)](h_BChw)
+                f_hat = f_hat + h_BChw
+                f_rest -= h_BChw
+                mean_vq_loss += F.mse_loss(f_hat.data, f_BChw).mul_(self.beta) + F.mse_loss(f_hat, f_no_grad)
+            
+            mean_vq_loss *= 1. / SN
+            f_hat = (f_hat.data - f_no_grad).add_(f_BChw)
+        return f_hat, mean_vq_loss
+
+    ##(criterion triple analysis)
+    def collect_eval_info(self, f_BChw: torch.Tensor):
+        dtype = f_BChw.dtype
+        if dtype != torch.float32: f_BChw = f_BChw.float()
+        B, C, H, W = f_BChw.shape
+        f_no_grad = f_BChw.detach()
+        
+        f_rest = f_no_grad.clone()
+        f_hat = torch.zeros_like(f_rest)
+
+        token_cat: List[torch.Tensor] = []
+        with torch.cuda.amp.autocast(enabled=False):
+            mean_vq_loss: torch.Tensor = 0.0
             SN = len(self.v_patch_nums)
             for si, pn in enumerate(self.v_patch_nums): # from small to large
                 # find the nearest embedding
@@ -47,35 +80,20 @@ class VectorQuantizer2(nn.Module):
                     d_no_grad.addmm_(rest_NC, self.embedding.weight.data.T, alpha=-2, beta=1)  # (B*h*w, vocab_size)
                     idx_N = torch.argmin(d_no_grad, dim=1)
                 
-                hit_V = idx_N.bincount(minlength=self.vocab_size).float()
-                if self.training:
-                    if dist.initialized(): handler = tdist.all_reduce(hit_V, async_op=True)
-                
+                token_cat.append(idx_N)
                 # calc loss
                 idx_Bhw = idx_N.view(B, pn, pn)
                 h_BChw = F.interpolate(self.embedding(idx_Bhw).permute(0, 3, 1, 2), size=(H, W), mode='bicubic').contiguous() if (si != SN-1) else self.embedding(idx_Bhw).permute(0, 3, 1, 2).contiguous()
                 h_BChw = self.quant_resi[si/(SN-1)](h_BChw)
                 f_hat = f_hat + h_BChw
                 f_rest -= h_BChw
-                
-                if self.training and dist.initialized():
-                    handler.wait()
-                    if self.record_hit == 0: self.ema_vocab_hit_SV[si].copy_(hit_V)
-                    elif self.record_hit < 100: self.ema_vocab_hit_SV[si].mul_(0.9).add_(hit_V.mul(0.1))
-                    else: self.ema_vocab_hit_SV[si].mul_(0.99).add_(hit_V.mul(0.01))
-                    self.record_hit += 1
-                vocab_hit_V.add_(hit_V)
-                mean_vq_loss += F.mse_loss(f_hat.data, f_BChw).mul_(self.beta) + F.mse_loss(f_hat, f_no_grad)
-            
-            mean_vq_loss *= 1. / SN
+               
             f_hat = (f_hat.data - f_no_grad).add_(f_BChw)
-        
-        margin = tdist.get_world_size() * (f_BChw.numel() / f_BChw.shape[1]) / self.vocab_size * 0.08
-        # margin = pn*pn / 100
-        if ret_usages: usages = [(self.ema_vocab_hit_SV[si] >= margin).float().mean().item() * 100 for si, pn in enumerate(self.v_patch_nums)]
-        else: usages = None
-        return f_hat, usages, mean_vq_loss
-
+            quant_error = F.mse_loss(f_hat.detach()-f_BChw.detach())
+            histogram = token_cat.bincount(minlength=self.vocab_size).float()
+            handler = tdist.all_reduce(histogram, async_op=True)
+            handler.wait()
+        return f_hat, quant_error, histogram
 
 class Phi(nn.Conv2d):
     def __init__(self, embed_dim, quant_resi):
@@ -86,16 +104,6 @@ class Phi(nn.Conv2d):
     def forward(self, h_BChw):
         return h_BChw.mul(1-self.resi_ratio) + super().forward(h_BChw).mul_(self.resi_ratio)
 
-
-class PhiShared(nn.Module):
-    def __init__(self, qresi: Phi):
-        super().__init__()
-        self.qresi: Phi = qresi
-    
-    def __getitem__(self, _) -> Phi:
-        return self.qresi
-
-
 class PhiPartiallyShared(nn.Module):
     def __init__(self, qresi_ls: nn.ModuleList):
         super().__init__()
@@ -105,20 +113,6 @@ class PhiPartiallyShared(nn.Module):
     
     def __getitem__(self, at_from_0_to_1: float) -> Phi:
         return self.qresi_ls[np.argmin(np.abs(self.ticks - at_from_0_to_1)).item()]
-    
-    def extra_repr(self) -> str:
-        return f'ticks={self.ticks}'
-
-
-class PhiNonShared(nn.ModuleList):
-    def __init__(self, qresi: List):
-        super().__init__(qresi)
-        # self.qresi = qresi
-        K = len(qresi)
-        self.ticks = np.linspace(1/3/K, 1-1/3/K, K) if K == 4 else np.linspace(1/2/K, 1-1/2/K, K)
-    
-    def __getitem__(self, at_from_0_to_1: float) -> Phi:
-        return super().__getitem__(np.argmin(np.abs(self.ticks - at_from_0_to_1)).item())
     
     def extra_repr(self) -> str:
         return f'ticks={self.ticks}'
--- VAR/model/var_substitution.py
+++ VAR/model/var_substitution.py
@@ -4,6 +4,7 @@ from torch import nn
 from einops import rearrange
 from torch.nn import functional as F
 from model.encoder_decoder import Encoder, Decoder
+from original_var_quantizer import Original_VAR
 from utils.util import Pack
 
 class VAR_Substitution(nn.Module):
@@ -21,7 +22,32 @@ class VAR_Substitution(nn.Module):
 
         self.quant_conv = torch.nn.Conv2d(32, 32, 3, stride=1, padding=1)
         self.post_quant_conv = torch.nn.Conv2d(32, 32, 3, stride=1, padding=1)
+
+        if self.args.VQ == "original_var":
+            self.quantizer = Original_VAR(vocab_size=4096, Cvae=32, using_znorm=False, beta=0.25, v_patch_nums=self.args.ms_token_size, quant_resi=0.5, share_quant_resi=4)
+
+    def forward(self, x):
+        pass
+
+    def collect_eval_info(self, x):
+        ## encoder
+        z = self.encoder(x)
+        z = self.pre_quant_proj(z)
+
+        if self.args.VQ == "var_no_vq":
+            z_q = z
+        else:
+            z_q, quant_error, histogram = self.quantizer.collect_eval_info(z)
+
+        z = self.post_quant_proj(z_q)
+        x_rec = self.decoder(z).clamp_(-1, 1)
+        rec_loss = F.mse_loss(x.contiguous(), x_rec.contiguous())
         
+        if self.args.VQ == "var_no_vq":
+            return x_rec, rec_loss
+        else:
+            return x_rec, rec_loss, quant_error, histogram  
+
 
 
 
