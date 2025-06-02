8697ec4201b264318fd9f5b09ebf3e72c75de911
sunset-clouds <xianghong_fang@163.com>
2025-06-03 07:51:12 +0800

    commit_initial

--- VAR/scripts/substitution/wasserstein_vq_multiscale.sh
+++ VAR/scripts/substitution/wasserstein_vq_multiscale.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=wasserstein_vq_p1
-#SBATCH --partition=long
+#SBATCH --job-name=wasserstein_vq_multiscale
+#SBATCH --partition=medium
 #SBATCH --nodes=1
 #SBATCH --mem=30gb
 #SBATCH --cpus-per-task 10
 #SBATCH --nodelist=g[003-009]
 #SBATCH --gpus-per-node=1
-#SBATCH --time=5-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --output /projects/yuanai/projects/VQ-Transplant/VAR/slurm/Substitution/ImageNet/wasserstein_vq_p1.out
 #SBATCH --error /projects/yuanai/projects/VQ-Transplant/VAR/slurm/Substitution/ImageNet/wasserstein_vq_p1.err
 
