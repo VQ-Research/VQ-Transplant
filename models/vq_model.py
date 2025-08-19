de5b7350812147b5941be12344ed081b77a439bd
sunset-clouds <xianghong_fang@163.com>
2025-08-19 11:21:48 +0800

    initial_commits

--- models/vq_model.py
+++ models/vq_model.py
@@ -95,16 +95,12 @@ class VQModel(nn.Module):
             pretrain_dict = torch.load(checkpoint_path, map_location='cpu')['model']
             encoder_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('encoder.')}
             decoder_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('decoder.')}
-            quant_conv_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('quant_conv.')}
-            post_quant_conv_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('post_quant_conv.')}
             quantizer_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('quantizer.')}
             projector_in_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('projector_in.')}
             projector_out_dict = {k: v for k, v in pretrain_dict.items() if k.startswith('projector_out.')}
 
             encoder_dict = {k.replace('encoder.', '', 1): v for k, v in encoder_dict.items()}
             decoder_dict = {k.replace('decoder.', '', 1): v for k, v in decoder_dict.items()}
-            quant_conv_dict = {k.replace('quant_conv.', '', 1): v for k, v in quant_conv_dict.items()}
-            post_quant_conv_dict = {k.replace('post_quant_conv.', '', 1): v for k, v in post_quant_conv_dict.items()}
             quantizer_dict = {k.replace('quantizer.', '', 1): v for k, v in quantizer_dict.items()}
             projector_in_dict = {k.replace('projector_in.', '', 1): v for k, v in projector_in_dict.items()}
             projector_out_dict = {k.replace('projector_out.', '', 1): v for k, v in projector_out_dict.items()}
@@ -112,13 +108,9 @@ class VQModel(nn.Module):
             self.encoder.load_state_dict(encoder_dict, strict=True)
             self.decoder.load_state_dict(decoder_dict, strict=True)
             self.quant_conv.load_state_dict(quant_conv_dict, strict=True)
-            self.post_quant_conv.load_state_dict(post_quant_conv_dict, strict=True)
-            self.quantizer.load_state_dict(quantizer_dict, strict=True)
             self.projector_in.load_state_dict(projector_in_dict, strict=True)
             self.projector_out.load_state_dict(projector_out_dict, strict=True)
             for param in self.encoder.parameters():
-                param.requires_grad = False
-            for param in self.quant_conv.parameters():
                 param.requires_grad = False
             for param in self.quantizer.parameters():
                 param.requires_grad = False
@@ -126,32 +118,28 @@ class VQModel(nn.Module):
                 param.requires_grad = False
             for param in self.projector_out.parameters():
                 param.requires_grad = False 
-            for param in self.post_quant_conv.parameters():
-                param.requires_grad = False
             for param in self.decoder.parameters():
                 param.requires_grad = True
             self.encoder.eval()
-            self.quantizer.eval()
             self.projector_in.eval()
             self.projector_out.eval()
             self.quant_conv.eval()
-            self.post_quant_conv.eval()
 
     def transplant(self, x):
         assert self.args.stage == "transplant"
         with torch.no_grad():
-            z = self.encoder(x)
-            zt = self.quant_conv(z)
-            z, _ = torch.chunk(zt, 2, dim=1)
+            ze = self.encoder(x)
+            zt = self.quant_conv(ze)
+            zm, _ = torch.chunk(zt, 2, dim=1)
+            z_obj = self.post_quant_conv(zm)
 
-        z_p = z + self.projector_in(zt)
+        z_p = self.projector_in(ze)
         z_q, vq_loss, utilization, perplexity = self.quantizer(z_p)
         z_q = z_q + self.projector_out(z_q)
 
-        loss = F.mse_loss(z_q, z.detach())
-        quant_error = F.mse_loss(z_q.detach(), z.detach())
+        loss = F.mse_loss(z_q, z_obj.detach())
+        quant_error = F.mse_loss(z_q.detach(), z_obj.detach())
         with torch.no_grad():
-            z_q = self.post_quant_conv(z_q)
             x_rec = self.decoder(z_q)
         rec_loss = F.mse_loss(x.contiguous(), x_rec.contiguous())
         transplant_loss = 2.0*loss + vq_loss
@@ -160,58 +148,44 @@ class VQModel(nn.Module):
     def refinement(self, x):
         assert self.args.stage == "refinement"
         with torch.no_grad():
-            z = self.encoder(x)
-            zt = self.quant_conv(z)
-            z, _ = torch.chunk(zt, 2, dim=1)
-
-            z_p = z + self.projector_in(zt)
+            ze = self.encoder(x)
+            z_p = self.projector_in(ze)
             z_q, _ = self.quantizer.collect_eval_info(z_p)
             z_q = z_q + self.projector_out(z_q)
-            z_q = self.post_quant_conv(z_q)
-
+            
         x_rec = self.decoder(z_q)
         return x_rec
 
     def collect_eval_info_transplant(self, x):
-        z = self.encoder(x)
-        zt = self.quant_conv(z)
-        z, _ = torch.chunk(zt, 2, dim=1)
+        ze = self.encoder(x)
+        zt = self.quant_conv(ze)
+        zm, _ = torch.chunk(zt, 2, dim=1)
+        z_obj = self.post_quant_conv(zm)
 
-        z_p = z + self.projector_in(zt)
+        z_p = self.projector_in(ze)
         z_q, histogram = self.quantizer.collect_eval_info(z_p)
         z_q = z_q + self.projector_out(z_q)
 
-        quant_error = F.mse_loss(z_q.detach(), z.detach())
-        z_q = self.post_quant_conv(z_q)
+        quant_error = F.mse_loss(z_q.detach(), z_obj.detach())
         x_rec = self.decoder(z_q).clamp_(-1, 1)
         rec_loss = F.mse_loss(x.contiguous(), x_rec.contiguous())
         return x_rec, rec_loss, quant_error, histogram
 
     def collect_eval_info_refinement(self, x):
-        z = self.encoder(x)
-        zt = self.quant_conv(z)
-        z, _ = torch.chunk(zt, 2, dim=1)
-
-        z_p = z + self.projector_in(zt)
+        ze = self.encoder(x)
+        z_p = self.projector_in(ze)
         z_q, _ = self.quantizer.collect_eval_info(z_p)
         z_q = z_q + self.projector_out(z_q)
 
-        z_q = self.post_quant_conv(z_q)
         x_rec = self.decoder(z_q).clamp_(-1, 1)
         rec_loss = F.mse_loss(x.contiguous(), x_rec.contiguous())
         return x_rec, rec_loss
 
     def reconstruction(self, x):
-        z = self.encoder(x)
-        zt = self.quant_conv(z)
-        z, _ = torch.chunk(zt, 2, dim=1)
-
-        z_p = z + self.projector_in(zt)
+        ze = self.encoder(x)
+        z_p = self.projector_in(ze)
         z_q = self.quantizer.collect_reconstruction(z_p)
         z_q = z_q + self.projector_out(z_q)
-
-        quant_error = F.mse_loss(z_q.detach(), z.detach())
-        z_q = self.post_quant_conv(z_q)
         x_rec = self.decoder(z_q).clamp_(-1, 1)
         return x_rec
 
