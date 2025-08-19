4e408d7ef2578d103ac34e5fa3d1753103d41089
sunset-clouds <xianghong_fang@163.com>
2025-08-20 07:24:02 +0800

    initial_commits

--- scripts/transplant/ema_var.sh
+++ scripts/transplant/ema_var.sh
@@ -13,4 +13,3 @@
 module load gcc opencv/4.8.1
 source /home/sunset/environment/VQ-Tokenizer/bin/activate
 CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12262 train_VAR_transplant.py --VQ=ema_vq --dataset_name=ImageNet --global_batch_size=128 --codebook_size 4096  --codebook_dim=16 --use_multiscale --stage=transplant --alpha=1.0 --beta=1.0 --gamma=0.0
-#CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12262 train_VAR_transplant.py --VQ=ema_vq --dataset_name=ImageNet --global_batch_size=128 --codebook_size 8192  --codebook_dim=16 --use_multiscale --stage=transplant --alpha=1.0 --beta=1.0 --gamma=0.0
--- scripts/transplant/mmd_vq_other.sh
+++ scripts/transplant/mmd_vq_other.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=aip-rudner
 #SBATCH --partition=gpubase_h100_b4
 #SBATCH --nodes=1
-#SBATCH --mem=50gb
+#SBATCH --mem=100gb
 #SBATCH --cpus-per-task 10
 #SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:h100:2
@@ -12,7 +12,6 @@
 
 module load gcc opencv/4.8.1
 source /home/sunset/environment/VQ-Tokenizer/bin/activate
-CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12255 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=Churches --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
-CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12255 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=Bedrooms --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
-CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12255 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=FFHQ --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
-CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12255 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=CelebAHQ --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12257 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=Churches --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12257 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=FFHQ --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12257 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=CelebAHQ --global_batch_size=128 --codebook_size 16384  --codebook_dim=16 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=1.0
