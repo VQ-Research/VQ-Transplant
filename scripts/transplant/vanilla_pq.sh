c56bb1d7dbe8629b5048eb723574f204bcff4c78
sunset-clouds <xianghong_fang@163.com>
2025-08-21 10:39:36 +0800

    initial_commits

---
+++ scripts/transplant/vanilla_pq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=vanilla_pq
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b4
+#SBATCH --nodes=1
+#SBATCH --mem=50gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=3-00:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/VQ-Transplant/slurm/Transplant/ImageNet/vanilla_pq.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/VQ-Transplant/slurm/Transplant/ImageNet/vanilla_pq.err
+
+module load gcc opencv/4.8.1
+source /home/sunset/environment/VQ-Tokenizer/bin/activate
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12272 train_PQ_transplant.py --VQ=vanilla_vq --dataset_name=ImageNet --global_batch_size=64 --codebook_size 256  --codebook_dim=8 --pq=2 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=0.0
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=12272 train_PQ_transplant.py --VQ=vanilla_vq --dataset_name=ImageNet --global_batch_size=64 --codebook_size 16  --codebook_dim=4 --pq=4 --stage=transplant --alpha=1.0 --beta=1.0 --gamma=0.0
--- train_PQ_transplant.py
+++ train_PQ_transplant.py
@@ -31,13 +31,14 @@ import ruamel.yaml as yaml
 import config
 from utils.util import Logger, LossManager, Pack, adjust_learning_rate, save_checkpoint
 from data import dataloader
-from models.tokenizer import VQModel
+from models.pq_model import PQModel
 from models.vq_loss import VQLoss
 from metric.metric import PSNR, LPIPS, SSIM
-from eval_tokenizer import eval_one_epoch
+from eval_tokenizer import eval_one_epoch_pq
 
 from timm.scheduler import create_scheduler_v2 as create_scheduler
 from utils.distributed import init_distributed_mode
+from eval_reconstruction import eval_reconstruction
 
 os.environ["TORCHDYNAMO_LOGLEVEL"] = "INFO"
 os.environ["TORCHDYNAMO_VERBOSE"] = "1" 
@@ -55,59 +56,92 @@ def main_worker(args):
     device = rank % torch.cuda.device_count()
     torch.cuda.set_device(device)
 
-    vq_model = VQModel(args)
+    pq_model = PQModel(args)
     total_para = 0
-    for p in vq_model.encoder.parameters():
+    for p in pq_model.encoder.parameters():
         total_para += p.numel()
-    for p in vq_model.decoder.parameters():
+    for p in pq_model.decoder.parameters():
         total_para += p.numel()
     print("VQ Model Parameters:", total_para)
-    
-    vq_model = vq_model.to(device)    
-    vq_model = nn.SyncBatchNorm.convert_sync_batchnorm(vq_model)
+    pq_model = pq_model.to(device)    
+    pq_model = nn.SyncBatchNorm.convert_sync_batchnorm(pq_model)
 
-    model_para = list(vq_model.encoder.parameters()) 
-    code_para = list(vq_model.quantizer.projector_in.parameters()) + list(vq_model.quantizer.projector_out.parameters())
-    optimizer = torch.optim.AdamW([{'params': model_para}, {'params': code_para, 'lr': 0.0001}], lr=args.lr_transplant, betas=(0.9, 0.95), weight_decay=0.0001)
+    if args.VQ == "wasserstein_vq" or args.VQ == "mmd_vq":
+        if args.pq == 2:
+            code_para = list(pq_model.quantizer1.embedding.parameters()) + list(pq_model.quantizer2.embedding.parameters()) 
+        elif args.pq == 4:
+            code_para = list(pq_model.quantizer1.embedding.parameters()) + list(pq_model.quantizer2.embedding.parameters()) + list(pq_model.quantizer3.embedding.parameters()) + list(pq_model.quantizer4.embedding.parameters()) 
+        model_para = list(pq_model.projector_in.parameters()) + list(pq_model.projector_out.parameters()) 
+        all_para = code_para + model_para
+        optimizer = torch.optim.AdamW([{'params': model_para}, {'params': code_para, 'lr': 0.01}], lr=args.lr_transplant, betas=(0.9, 0.95), weight_decay=0.00001)
+    elif args.VQ == "vanilla_vq" or args.VQ == "online_vq":
+        if args.pq == 2:
+            model_para = list(pq_model.quantizer1.embedding.parameters()) + list(pq_model.quantizer2.embedding.parameters()) + list(pq_model.projector_in.parameters()) + list(pq_model.projector_out.parameters()) 
+        elif args.pq == 4:
+            model_para = list(pq_model.quantizer1.embedding.parameters()) + list(pq_model.quantizer2.embedding.parameters()) + list(pq_model.quantizer3.embedding.parameters()) + list(pq_model.quantizer4.embedding.parameters()) + list(pq_model.projector_in.parameters()) + list(pq_model.projector_out.parameters()) 
+        optimizer = torch.optim.AdamW(model_para, lr=args.lr_transplant, betas=(0.9, 0.95), weight_decay=0.00001)
+    elif args.VQ == "ema_vq":
+        model_para = list(pq_model.projector_in.parameters()) + list(pq_model.projector_out.parameters()) 
+        optimizer = torch.optim.AdamW(model_para, lr=args.lr_transplant, betas=(0.9, 0.95), weight_decay=0.00001)
+
     train_dataloader, val_dataloader, train_sampler, len_train_set, len_val_set = build_dataloader(args)
+    pq_model = DDP(pq_model.to(device), device_ids=[args.gpu], find_unused_parameters=False)
+    pq_model.train()
+    pq_model.module.encoder.eval()
+    pq_model.module.decoder.eval()
+    pq_model.module.quant_conv.eval()
+    pq_model.module.post_quant_conv.eval()
 
-    vq_model = DDP(vq_model.to(device), device_ids=[args.gpu], find_unused_parameters=False)
-    vq_model.train()
-
-    results_eval = {'epoch':[], 'psnr':[], 'ssim':[], 'lpips':[], 'rec_loss': [], 'vq_loss': [], 'prob_commit_loss':[], 'entropy_loss':[], 'avg_entropy_loss':[]}
+    results_eval = {'epoch':[], 'psnr':[], 'ssim':[], 'lpips':[], 'rec_loss': [], 'quant_error': [], 'utilization': [], 'perplexity': []}
     train_loss = LossManager()
     print("Start training...")
     start_epoch = 1 
+    total_steps = len(train_dataloader)*args.transplant_epochs
     for epoch in range(start_epoch, args.transplant_epochs+1):
         train_sampler.set_epoch(epoch)
         print("epoch:%d, cur_lr:%4f"%(epoch, optimizer.param_groups[0]["lr"]))
-    
         start_time = time.time()
         for step, (x, _) in enumerate(train_dataloader):
             cur_iter = len(train_dataloader) * (epoch-1) + step
+            lr = adjust_learning_rate(optimizer, cur_iter, total_steps, args.lr_transplant)
             with torch.autocast(device_type='cuda', dtype=torch.float32):
                 x = x.to(device, non_blocking=True)
-
                 optimizer.zero_grad()
-                transplant_loss, rec_loss, vq_loss, categorical_loss, prob_commit_loss, entropy_loss, avg_entropy_loss = vq_model.module.transplant(x, cur_iter)
-                info_pack = Pack(transplant_loss=transplant_loss, rec_loss=rec_loss, vq_loss=vq_loss, categorical_loss=categorical_loss, prob_commit_loss=prob_commit_loss, entropy_loss=entropy_loss, avg_entropy_loss=avg_entropy_loss)
-                
-                transplant_loss.backward()
-                torch.nn.utils.clip_grad_norm_(model_para, 1.0)
-                optimizer.step()
 
+                transplant_loss, rec_loss, quant_error, utilization, perplexity = pq_model.module.transplant(x)
+                info_pack = Pack(transplant_loss=transplant_loss, rec_loss=rec_loss, quant_error=quant_error, utilization=utilization, perplexity=perplexity)
+                transplant_loss.backward()
+                if args.VQ == "wasserstein_vq":
+                    has_nan = False            
+                    for param in all_para:
+                        if param.grad is not None and (torch.isnan(param.grad).any() or torch.isinf(param.grad).any()):
+                            has_nan = True
+                            break
+                    if has_nan == False:
+                        torch.nn.utils.clip_grad_norm_(all_para, 1.0)
+                        optimizer.step()
+                    else:
+                        print("skip gradient update!")
+                elif args.VQ == "mmd_vq":
+                    torch.nn.utils.clip_grad_norm_(all_para, 1.0)
+                    optimizer.step()
+                else:
+                    torch.nn.utils.clip_grad_norm_(model_para, 1.0)
+                    optimizer.step()
+                    
             train_loss.add_loss(info_pack)
             if int(os.environ['LOCAL_RANK']) == 0 and (step+1) %10 ==0:
                 print(train_loss.pprint(window=50, prefix='Train Epoch: [{}/{}] Iters:[{}/{}]'.format(epoch, args.transplant_epochs, step+1, len(train_dataloader))))
 
         train_loss.clear()
         if epoch % args.eval_epochs == 0 and int(os.environ['LOCAL_RANK']) == 0:
-            vq_model.train()
+            pq_model.train()
             checkpoint_path = os.path.join(args.checkpoint_dir, 'checkpoint-'+args.saver_name_pre+'-'+str(epoch)+'.pth.tar')
-            save_checkpoint({'epoch': epoch, 'model': vq_model.module.state_dict(), 'optimizer': optimizer.state_dict(), 'args': args}, is_best=False, filename=checkpoint_path) 
+            save_checkpoint({'epoch': epoch, 'model': pq_model.module.state_dict(), 'optimizer': optimizer.state_dict(), 'args': args}, is_best=False, filename=checkpoint_path) 
+        
         if epoch % args.eval_epochs == 0:
             with torch.no_grad():
-                results_pack = eval_one_epoch(args, vq_model, epoch, val_dataloader, len_val_set)
+                results_pack = eval_one_epoch_pq(args, pq_model, epoch, val_dataloader, len_val_set)
 
             if int(os.environ['LOCAL_RANK']) == 0:
                 results_eval['epoch'].append(epoch)
@@ -115,25 +149,25 @@ def main_worker(args):
                 results_eval['ssim'].append(results_pack.ssim)
                 results_eval['lpips'].append(results_pack.lpips)
                 results_eval['rec_loss'].append(results_pack.rec_loss)
-                results_eval['vq_loss'].append(results_pack.vq_loss)
-                results_eval['prob_commit_loss'].append(results_pack.prob_commit_loss)
-                results_eval['entropy_loss'].append(results_pack.entropy_loss)
-                results_eval['avg_entropy_loss'].append(results_pack.avg_entropy_loss)
+                results_eval['quant_error'].append(results_pack.quant_error)
+                results_eval['utilization'].append(results_pack.utilization)
+                results_eval['perplexity'].append(results_pack.perplexity)
                 
                 results_val_len = len(results_eval['epoch'])
                 data_frame = pd.DataFrame(data=results_eval, index=range(1, results_val_len+1))
                 data_frame.to_csv('{}/eval_{}_rec_results.csv'.format(args.results_dir, args.saver_name_pre), index_label='index')
 
     print("######### saving checkpoint #########")
-    vq_model.train() 
+    pq_model.train() 
     if int(os.environ['LOCAL_RANK']) == 0:
         checkpoint_path = os.path.join(args.checkpoint_dir, 'checkpoint-'+args.saver_name_pre+'.pth.tar')
-        save_checkpoint({'epoch': epoch, 'model': vq_model.module.state_dict(), 'optimizer': optimizer.state_dict(), 'args': args}, is_best=False, filename=checkpoint_path) 
-
-    vq_model.eval() 
+        save_checkpoint({'epoch': epoch, 'model': pq_model.module.state_dict(), 'optimizer': optimizer.state_dict(), 'args': args}, is_best=False, filename=checkpoint_path) 
+        eval_reconstruction(args, pq_model)
+    pq_model.eval() 
     dist.destroy_process_group()
 
 if __name__ == '__main__':
+    os.environ['NCCL_TIMEOUT_IN_MS'] = '7200000'
     args = config.parse_arg()
     dict_args = vars(args)
     sys.stdout = Logger(args.saver_dir, args.saver_name_pre)
