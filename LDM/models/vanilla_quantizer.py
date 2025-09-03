78f164c9c795b70c7cfcaa6207e8c1f710eaddd8
sunset-clouds <xianghong_fang@163.com>
2025-09-03 11:01:07 +0800

    initial_commits

--- LDM/models/vanilla_quantizer.py
+++ LDM/models/vanilla_quantizer.py
@@ -80,47 +80,38 @@ class VanillaProductQuantizer(ProductQuantizer):
     def __init__(self, args):
         super().__init__(args)
         self.args = args
-        self.overall_codebook_size = args.codebook_size * args.codebook_size
+        self.total_codebook_size = args.codebook_size * args.codebook_size
+
+    def sub_quantizer(self, z_enc, codebook):
+        B, C, H, W = z_enc.shape
+        z = rearrange(z_enc, 'b c h w -> b h w c') 
+        z_flat = z.reshape(-1, C).contiguous()  
+
+        # distances from z to embeddings
+        d = torch.sum(z_flat ** 2, dim=1, keepdim=True) + \
+            torch.sum(codebook.weight.data**2, dim=1) - 2 * \
+            torch.matmul(z_flat, codebook.weight.data.t())
+
+        token = torch.argmin(d, dim=1)
+        z_dec = codebook(token).view(z.shape).permute(0, 3, 1, 2).contiguous()
+        return z_dec, token
 
     def forward(self, z_enc):
         ### chunk z_enc to z_enc_p1, z_enc_p2
         z_enc_1, z_enc_2 = torch.chunk(z_enc, 2, dim=1)
-
-        # reshape z_enc -> (batch, height, width, channel) and flatten
-        #z, 'b c h w -> b h w c'
-        B, C, H, W = z_enc_1.shape
-        z_1 = rearrange(z_enc_1, 'b c h w -> b h w c') 
-        z_flat_1 = z_1.reshape(-1, C).contiguous()  
-
-        B, C, H, W = z_enc_2.shape
-        z_2 = rearrange(z_enc_2, 'b c h w -> b h w c') 
-        z_flat_2 = z_2.reshape(-1, C).contiguous()  
-        
-        # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
-        d_1 = z_flat_1.detach().pow(2).sum(dim=1, keepdim=True) + \
-            self.embedding_1.weight.data.pow(2).sum(dim=1) - 2 * \
-            torch.einsum('bd,nd->bn', z_flat_1.detach(), self.embedding_1.weight.data) # 'n d -> d n'
-
-        d_2 = z_flat_2.detach().pow(2).sum(dim=1, keepdim=True) + \
-            self.embedding_2.weight.data.pow(2).sum(dim=1) - 2 * \
-            torch.einsum('bd,nd->bn', z_flat_2.detach(), self.embedding_2.weight.data) # 'n d -> d n'
-
-        token_1 = torch.argmin(d_1, dim=1)
-        z_dec_1 = self.embedding(token_1).view(z_1.shape).permute(0, 3, 1, 2).contiguous()
-
-        token_2 = torch.argmin(d_2, dim=1)
-        z_dec_2 = self.embedding(token_2).view(z_2.shape).permute(0, 3, 1, 2).contiguous()
+        z_dec_1, token_1 = self.sub_quantizer(z_enc_1, self.embedding_1)
+        z_dec_2, token_2 = self.sub_quantizer(z_enc_2, self.embedding_2)
 
         token = token_1 + token_2 * self.args.codebook_size
         z_dec = torch.cat((z_dec_1, z_dec_2), dim=1)
         commit_loss = self.beta * F.mse_loss(z_dec.detach(), z_enc) + self.alpha * F.mse_loss(z_dec, z_enc.detach())
 
-        histogram = token.bincount(minlength=self.overall_codebook_size).float()
+        histogram = token.bincount(minlength=self.total_codebook_size).float()
         handler = tdist.all_reduce(histogram, async_op=True)
         handler.wait()
 
         codebook_usage_counts = (histogram > 0).float().sum()
-        utilization = codebook_usage_counts.item() / self.overall_codebook_size
+        utilization = codebook_usage_counts.item() / self.total_codebook_size
             
         avg_probs = histogram/histogram.sum(0)
         perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
