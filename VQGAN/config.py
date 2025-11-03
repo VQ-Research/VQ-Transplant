c1844826ae09d01bfa047f35f24aa829270c3083
sunset-clouds <xianghong_fang@163.com>
2025-11-04 07:01:41 +0800

    initial_commits

--- VQGAN/config.py
+++ VQGAN/config.py
@@ -44,7 +44,7 @@ def parse_arg():
     parser.add_argument('--disc_cr_loss_weight', type=float, default=4.0, help='refinement stage: disc_cr_loss_weight')
 
     ### Training Configuration
-    parser.add_argument('--VQ', default='wasserstein_vq', help='various vq approaches.', choices=['wasserstein_vq', 'vanilla_vq', 'ema_vq', 'online_vq', 'mmd_vq', 'original_ldm', 'bsq', 'fsq', 'lfq'])
+    parser.add_argument('--VQ', default='wasserstein_vq', help='various vq approaches.', choices=['wasserstein_vq', 'vanilla_vq', 'ema_vq', 'online_vq', 'mmd_vq', 'bsq', 'fsq', 'lfq'])
     parser.add_argument('--use_multiscale', action='store_true', help='False: employ single VQ; True: use multiscale-VQ as original VAR.')
     parser.add_argument('--transplant_epochs', type=int, default=2, help="training epochs, 5 epochs for transplant stage.")
     parser.add_argument('--refinement_epochs', type=int, default=5, help="training epochs, 5 epochs for refinement stage.")
@@ -126,25 +126,19 @@ def parse_arg():
     args.data_pre = '{}'.format(args.dataset_name)
 
     ### model prefix 
-    if args.VQ == "original_ldm":
-        args.model_pre = 'model_'
-    elif args.VQ == "wasserstein_vq" or args.VQ == "vanilla_vq" or args.VQ == "ema_vq" or args.VQ == "online_vq" or args.VQ == "mmd_vq":
+    if args.VQ == "wasserstein_vq" or args.VQ == "vanilla_vq" or args.VQ == "ema_vq" or args.VQ == "online_vq" or args.VQ == "mmd_vq":
         args.model_pre = 'model_{}_{}_{}'.format(args.codebook_size, args.codebook_dim, args.pq)
     else:
         args.model_pre = 'model_{}_{}'.format(args.project_dim, args.L)
     
     ### loss prefix 
-    if args.VQ == "original_ldm":
-        args.loss_pre = 'loss_'
-    elif args.VQ == "wasserstein_vq" or args.VQ == "vanilla_vq" or args.VQ == "ema_vq" or args.VQ == "online_vq" or args.VQ == "mmd_vq":
+    if args.VQ == "wasserstein_vq" or args.VQ == "vanilla_vq" or args.VQ == "ema_vq" or args.VQ == "online_vq" or args.VQ == "mmd_vq":
         args.loss_pre = 'loss_{}_{}_{}_{}'.format(args.alpha, args.beta, args.gamma, args.disc_weight)
     else:
         args.loss_pre = 'loss_{}_{}'.format(args.beta, args.disc_weight)
 
     ### train prefix 
-    if args.VQ == "original_ldm":
-        args.training_pre = '{}'.format(args.VQ)
-    elif args.VQ == "wasserstein_vq" or args.VQ == "vanilla_vq" or args.VQ == "ema_vq" or args.VQ == "online_vq" or args.VQ == "mmd_vq":
+    if args.VQ == "wasserstein_vq" or args.VQ == "vanilla_vq" or args.VQ == "ema_vq" or args.VQ == "online_vq" or args.VQ == "mmd_vq":
         args.training_pre = '{}_{}_{}'.format(args.VQ, args.stage, args.use_multiscale)
     else:
         args.training_pre = '{}_{}'.format(args.VQ, args.stage)
